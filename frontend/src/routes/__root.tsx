418c62eab442d4cba82d19f1aa2c7dcdc82cd7dd
Mateus Fiamenghi <mbfiamenghi@gmail.com>
2025-06-14 19:59:22 -0700

    add shadcn and theme toggle

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,11 +1,13 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { ModeToggle } from "@/components/mode-toggle";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <ModeToggle />
       <Outlet />
       <TanStackRouterDevtools />
     </>
   ),
-})
+});
